ce6a7195703c96598b08c56a9fa1d00304790d9d
lunash <101923529+lunash0@users.noreply.github.com>
2023-06-12 05:46:11 +0900

    Create ft_boolean.h

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunmoon <seunmoon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 02:41:40 by seunmoon          #+#    #+#             */
+/*   Updated: 2023/03/02 12:15:57 by seunmoon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+# define EVEN(nbr) ((nbr) % 2 == 0)
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+typedef int	t_bool;
+
+#endif
