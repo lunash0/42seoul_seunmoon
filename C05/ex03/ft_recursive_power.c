83c9386b35716cfc80bec4d7ff1f962e9209464b
lunash <101923529+lunash0@users.noreply.github.com>
2023-06-12 05:40:54 +0900

    Create ft_recursive_power.c

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunmoon <seunmoon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 09:24:58 by seunmoon          #+#    #+#             */
+/*   Updated: 2023/02/27 00:08:36 by seunmoon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (nb * ft_recursive_power(nb, power - 1));
+}
