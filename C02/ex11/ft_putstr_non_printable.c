c29265fa90cbccd5554f99ed686bc0d856e3805c
lunash <101923529+lunash0@users.noreply.github.com>
2023-06-12 05:30:56 +0900

    Create ft_putstr_non_printable.c

---
+++ C02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunmoon <seunmoon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 06:24:19 by seunmoon          #+#    #+#             */
+/*   Updated: 2023/02/20 20:52:43 by seunmoon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_hexa(unsigned char c)
+{
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	write(1, "\\", 1);
+	write(1, &hex[c / 16], 1);
+	write(1, &hex[c % 16], 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 32 && str[i] <= 126)
+		{
+			ft_putchar(str[i]);
+		}
+		else
+			ft_hexa((unsigned char)str[i]);
+		i++;
+	}
+}
