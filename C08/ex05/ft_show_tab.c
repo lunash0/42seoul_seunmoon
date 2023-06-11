11a3439ed4288e4ea6a4d8c39e581fb575574873
lunash <101923529+lunash0@users.noreply.github.com>
2023-06-12 05:47:06 +0900

    Create ft_show_tab.c

---
+++ C08/ex05/ft_show_tab.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunmoon <seunmoon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 19:12:36 by seunmoon          #+#    #+#             */
+/*   Updated: 2023/03/02 12:15:54 by seunmoon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stock_str.h"
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	write(1, str, len);
+}
+
+void	rec(int nb)
+{
+	char	num;
+
+	if ((nb) != 0)
+	{
+		num = nb % 10 + '0';
+		nb /= 10;
+		rec(nb);
+		write(1, &num, 1);
+	}
+}
+
+void	ft_putnbr(int n)
+{
+	if (n == -2147483648)
+		write(1, "-2147483648", 11);
+	else if (n == 0)
+		write(1, "0", 1);
+	else if (n < 0)
+	{
+		write(1, "-", 1);
+		rec(n * -1);
+	}
+	else
+		rec(n);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str)
+	{
+		ft_putstr(par[i].str);
+		write(1, "\n", 1);
+		ft_putnbr(par[i].size);
+		write(1, "\n", 1);
+		ft_putstr(par[i].copy);
+		write(1, "\n", 1);
+		i++;
+	}
+}
