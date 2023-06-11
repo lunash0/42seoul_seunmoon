1b55e3c019e890bfacd17738ec9bbf759e9310bf
lunash <101923529+lunash0@users.noreply.github.com>
2023-06-12 05:52:06 +0900

    Create calculate.c

---
+++ C11/ex05/calculate.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calculate.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunmoon <seunmoon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 21:56:32 by seunmoon          #+#    #+#             */
+/*   Updated: 2023/03/02 21:05:52 by seunmoon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	plus(int n1, int n2)
+{
+	return (n1 + n2);
+}
+
+int	minus(int n1, int n2)
+{
+	return (n1 - n2);
+}
+
+int	mul(int n1, int n2)
+{
+	return (n1 * n2);
+}
+
+int	div(int n1, int n2)
+{
+	return (n1 / n2);
+}
+
+int	mod(int n1, int n2)
+{
+	return (n1 % n2);
+}
