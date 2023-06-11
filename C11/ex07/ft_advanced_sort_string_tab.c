145a6ecad28f42ccca388da50f1213dfd0825e24
lunash <101923529+lunash0@users.noreply.github.com>
2023-06-12 05:53:19 +0900

    Create ft_advanced_sort_string_tab.c

---
+++ C11/ex07/ft_advanced_sort_string_tab.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_advanced_sort_string_tab.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunmoon <seunmoon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 11:47:13 by seunmoon          #+#    #+#             */
+/*   Updated: 2023/03/02 20:09:10 by seunmoon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_advanced_sort_string_tab(char **tab, int (*cmp)(char *, char *))
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 0;
+	while (tab[i] != 0)
+	{
+		j = i;
+		while (tab[j] != 0)
+		{
+			if (cmp(tab[i], tab[j]) > 0)
+			{
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
