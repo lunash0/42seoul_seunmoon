f4a018b6d5dd11d1099ab72ca830ac4fb565c017
lunash <101923529+lunash0@users.noreply.github.com>
2023-06-12 05:55:07 +0900

    Create rush04.c

---
+++ rush00/rush04.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunmoon <seunmoon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/11 14:50:53 by jinhole2          #+#    #+#             */
+/*   Updated: 2023/02/12 12:56:15 by jinhole2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_abbc(int i)
+{
+	char	a;
+	char	c;
+	char	b;
+	int		k;
+
+	c = 'C';
+	b = 'B';
+	a = 'A';
+	k = 1;
+	if (i == 1)
+	{
+		ft_putchar(a);
+		return ;
+	}
+	ft_putchar(a);
+	while (k <= i - 2)
+	{
+		ft_putchar(b);
+		k++;
+	}
+	ft_putchar(c);
+}
+
+void	ft_bbbb(int i)
+{
+	char	b;
+	int		k;
+
+	b = 'B';
+	k = 1;
+	if (i == 1)
+	{
+		ft_putchar(b);
+		return ;
+	}
+	ft_putchar(b);
+	while (k <= i - 2)
+	{
+		ft_putchar(' ');
+		k++;
+	}
+	ft_putchar(b);
+}
+
+void	ft_cbba(int i)
+{
+	char	a;
+	char	b;
+	char	c;
+	int		k;
+
+	c = 'C';
+	a = 'A';
+	b = 'B';
+	k = 1;
+	if (i == 1)
+	{
+		ft_putchar(c);
+		return ;
+	}
+	ft_putchar(c);
+	while (k <= i - 2)
+	{
+		ft_putchar(b);
+		k++;
+	}
+	ft_putchar(a);
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	i = 1;
+	while (i <= y)
+	{
+		if (i == 1)
+			ft_abbc(x);
+		else if (i == y)
+			ft_cbba(x);
+		else
+			ft_bbbb(x);
+		i++;
+		write(1, "\n", 1);
+	}
+}
