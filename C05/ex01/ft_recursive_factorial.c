20f1f8a3e1bb1b720e0a418e7218fefc5d11edf0
lunash <101923529+lunash0@users.noreply.github.com>
2023-06-12 05:40:26 +0900

    Create ft_recursive_factorial.c

---
+++ C05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunmoon <seunmoon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 03:00:18 by seunmoon          #+#    #+#             */
+/*   Updated: 2023/02/27 16:26:44 by seunmoon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb <= 1)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
