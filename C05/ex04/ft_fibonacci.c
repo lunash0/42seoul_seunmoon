87d1b0fd75cc4676574855e8047de0486e600129
lunash <101923529+lunash0@users.noreply.github.com>
2023-06-12 05:41:14 +0900

    Create ft_fibonacci.c

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunmoon <seunmoon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 09:53:41 by seunmoon          #+#    #+#             */
+/*   Updated: 2023/02/22 11:59:52 by seunmoon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1 || index == 2)
+		return (1);
+	else
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
