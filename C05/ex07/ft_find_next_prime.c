4aaf83ac0231c3c8549a579693480504a77d39df
lunash <101923529+lunash0@users.noreply.github.com>
2023-06-12 05:42:00 +0900

    Create ft_find_next_prime.c

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunmoon <seunmoon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 15:46:35 by seunmoon          #+#    #+#             */
+/*   Updated: 2023/02/27 00:10:15 by seunmoon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	long long	i;
+
+	i = 2;
+	if (nb == 0 || nb == 1)
+		return (0);
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (nb <= 1)
+		return (2);
+	else if (ft_is_prime(nb))
+		return (nb);
+	else
+		return (ft_find_next_prime(nb + 1));
+}
