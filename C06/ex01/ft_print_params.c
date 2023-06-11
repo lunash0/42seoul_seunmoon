28dee4f529d66a3c028749cf45f0111c05f901e7
lunash <101923529+lunash0@users.noreply.github.com>
2023-06-12 05:42:50 +0900

    Create ft_print_params.c

---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunmoon <seunmoon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 21:32:30 by seunmoon          #+#    #+#             */
+/*   Updated: 2023/02/24 03:16:01 by seunmoon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int ac, char *av[])
+{
+	int	i;
+
+	i = 1;
+	while (i < ac)
+	{
+		ft_putstr(av[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
